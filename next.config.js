c4f3e5aeb51b6cef5306ed97b60554325945e213
Fiona <fiona20072007@gmail.com>
2021-04-10 23:34:54 +0800

    add mdx setting and frontmatter setting

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const withPlugins = require('next-compose-plugins');
+
+const withMDX = require('@next/mdx');
+const optimizedImages = require('next-optimized-images');
+
+const detectFrontmatter = require('remark-frontmatter');
+const extractFrontmatter = require('./lib/frontmatter/extractFrontmatter.js');
+
+module.exports = withPlugins([
+    [withMDX({
+        extension: /\.mdx?$/,
+        options: {
+            remarkPlugins: [
+                [detectFrontmatter, ['yaml']],
+                extractFrontmatter,
+            ],
+        },
+    })],
+    [optimizedImages, {}],
+], {
+    pageExtensions: ['js', 'mdx'],
+});
